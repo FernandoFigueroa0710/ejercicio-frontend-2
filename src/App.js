dc24779c12b03b27e546f92ecb020df826380209
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2018-11-16 11:49:25 -0600

    update: align data in columns

--- src/App.js
+++ src/App.js
@@ -10,11 +10,39 @@ class App extends Component {
     };
   }
   render() {
-    console.log(this.state.list);
     return (
       <div>
         <h1 className="header">Resuelve Employee List</h1>
-        <EmployeeList employeeList={this.state.list} />
+        <table role="grid">
+          <thead>
+            <tbody>
+              <th width="200" scope="column" id="name">
+                Name
+              </th>
+
+              <th width="175" scope="column" id="company">
+                Company
+              </th>
+
+              <th width="100" scope="column" id="age">
+                Age
+              </th>
+
+              <th width="175" scope="column" id="phone">
+                Phone
+              </th>
+
+              <th width="200" scope="column" id="email">
+                Email
+              </th>
+
+              <th width="100" scope="column" id="salary">
+                Salary
+              </th>
+            </tbody>
+          </thead>
+          <EmployeeList employeeList={this.state.list} />
+        </table>
       </div>
     );
   }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,18 +1,20 @@
 import React from "react";
 
 const Employee = props => (
-  <div>
-    {`${props.employee.name}`}
-    <br />
-    {`${props.employee.company}`}
-    <br />
-    {`${props.employee.salary}`}
-    <br />
-    {`${props.employee.age}`}
-    <br />
-    {`${props.employee.phone}`}
-    <br />
-    {`${props.employee.email}`}
-  </div>
+  <tbody>
+    <tr>
+      <td width="200" headers="name">{`${props.employee.name}`}</td>
+
+      <td width="175" headers="company">{`${props.employee.company}`}</td>
+
+      <td width="100" headers="age">{`${props.employee.age}`}</td>
+
+      <td width="175" headers="phone">{`${props.employee.phone}`}</td>
+
+      <td width="200" headers="email">{`${props.employee.email}`}</td>
+
+      <td width="100" headers="salary">{`${props.employee.salary}`}</td>
+    </tr>
+  </tbody>
 );
 export default Employee;
